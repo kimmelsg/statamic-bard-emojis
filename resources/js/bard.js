372a9b149d86947bb78c56a6e8dad5b7246b3820
Ethan Knowlton <eknowlton@gmail.com>
2021-07-06 14:55:35 -0400

    playing with menu

--- resources/js/bard.js
+++ resources/js/bard.js
@@ -1,5 +1,7 @@
 import EmojisMenu from "./EmojisMenu";
+import Emoji from "./Emoji";
 
+Statamic.$bard.extend(({ node }) => node(new Emoji()));
 Statamic.$bard.buttons(buttons => {
   const indexOfBold = _.findIndex(buttons, { command: "bold" });
   buttons.splice(indexOfBold, 0, {
@@ -10,40 +12,4 @@ Statamic.$bard.buttons(buttons => {
     icon: "paint-brush",
     component: EmojisMenu
   });
-});
-
-const EmojiPlugin = function () {
-  return function (scribe) {
-    var command = new scribe.api.command("emoji");
-
-    scribe.commands.emoji = command;
-
-    command.execute = function (param) {
-      console.log(param);
-      return;
-      scribe.transactionManager.run(() => {
-        const sel = new scribe.api.Selection();
-        const range = sel.range;
-
-        sel.placeMarkers();
-
-        const contents = range.extractContents();
-
-        contents.childNodes.forEach(
-          node => (node.textContent = node.textContent.toUpperCase())
-        );
-
-        range.insertNode(contents);
-
-        sel.selection.removeAllRanges();
-        sel.selection.addRange(range);
-
-        sel.removeMarkers();
-      });
-    };
-  };
-};
-
-Statamic.booting(() => {
-    Statamic.$bard.plugins.push(EmojiPlugin);
 });
